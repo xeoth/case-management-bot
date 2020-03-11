d920989896fdfa555861b0a92d8e99433485a520
Xeoth <ujan.pl@gmail.com>
2020-03-11 22:25:03 +0100

    Added allowed hooks

--- src/events/message.js
+++ src/events/message.js
@@ -1,7 +1,17 @@
 const Discord = require('discord.js');
 
+// allowed webhook IDs
+const allowedHooks = [
+  '533373537755332610',
+  '687231668062388234'
+];
+
 module.exports = async (client, message) => {
-  if (message.author.bot) return;
+
+  if (message.author.bot && !allowedHooks.includes(message.webhookID)) {
+    return;
+  }
+
   if (message.content.indexOf(client.config.prefix) !== 0) return;
   const prefixLen = client.config.prefix.length;
   const args = message.content.slice(prefixLen).trim().split(/ +/g);
